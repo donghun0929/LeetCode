f33fa7a5262850890d3b24f2dd0152a3480f1532
donghun0929 <94956813+donghun0929@users.noreply.github.com>
2022-09-29 20:06:01 +0900

    Time: 60 ms (5.44%), Space: 81.8 MB (5.15%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,98 +1,37 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        stack <int> s;
-        int retVal = 0;
-        int high = 0;
-        if(height.size()==1)
-            return 0;
+        
+        vector<int> leftVal(20001);
+        vector<int> rightVal(20001);
+        
+        int leftMax = 0;
+        int rightMax = 0;
+        int val=0;
+        
+        for(int L = 0; L < height.size(); L++)
+        {
+            int H = height[L];
+            leftMax = leftMax > H ? leftMax : H;
+            if(L == 0)
+                continue;
+            leftVal[L] = leftMax - H;            
+        }
+        
+        for(int R= height.size()-1; R >=0; R--)
+        {
+            int H = height[R];
+            rightMax = rightMax > H ? rightMax : H;
+            if(R == height.size()-1)
+                continue;
+            rightVal[R] = rightMax - H;            
+        }
         
         for(int i = 0; i < height.size(); i++)
         {
-            high = height[high] > height[i] ? high : i;
+            int waterDeep = min(rightVal[i],leftVal[i]);
+            val += waterDeep;
         }
-        
-        retVal += LeftStack(height,high);
-        retVal += RightStack(height,high);
-        
-        return retVal;
-    }
-    
-    int LeftStack(vector<int>& height, int Right)
-    {
-        stack <int> s;
-        int retVal = 0;
-        int maxNum = height[0];  
-        int cnt = 1;
-        s.push(maxNum);
-        for(int i = 1; i <= Right; i++)
-        {
-            
-            if(maxNum <= height[i] && cnt > 0)
-            {
-                s.pop();
-                s.push(height[i]);
-                maxNum =  height[i];
-                cnt++;
-            }
-            else if(maxNum <= height[i] && cnt == 0)
-            {
-                while(!s.empty())
-                {
-                    int k = s.top();
-                    s.pop();
-                    retVal = retVal + (maxNum - k);
-                }
-                s.push(height[i]);
-                maxNum = height[i];
-                cnt++;
-            }
-            if(maxNum > height[i])
-            {
-                s.push(height[i]);
-                cnt = 0;
-            }
-        }
-        
-        return retVal;
-    }
-    
-    int RightStack(vector<int>& height, int Left)
-    {
-        stack <int> s;
-        int retVal = 0;
-        int maxNum = height[height.size()-1];  
-        int cnt = 1;
-        s.push(maxNum);
-        for(int i = height.size()-1; i >= Left ; i--)
-        {
-            
-            if(maxNum <= height[i] && cnt > 0)
-            {
-                s.pop();
-                s.push(height[i]);
-                maxNum =  height[i];
-                cnt++;
-            }
-            else if(maxNum <= height[i] && cnt == 0)
-            {
-                while(!s.empty())
-                {
-                    int k = s.top();
-                    s.pop();
-                    retVal = retVal + (maxNum - k);
-                }
-                s.push(height[i]);
-                maxNum = height[i];
-                cnt++;
-            }
-            if(maxNum > height[i])
-            {
-                s.push(height[i]);
-                cnt = 0;
-            }
-        }
-        
-        return retVal;
+        return val;
     }
 };
